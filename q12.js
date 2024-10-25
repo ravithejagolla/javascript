416fb533b421b7b35f86d6ae2edde73b9f010e9c
ravithejagolla <gollaravitheja007@gmail.com>
2024-10-25 21:16:12 +0530

    addeD

---
+++ q12.js
@@ -0,0 +1,27 @@
+let num1=20;
+let num2=10
+
+console.log(num1+num2)  //addition
+
+console.log(num1-num2)  //substraction
+
+console.log(num1*num2)  //Multiplication
+
+if(num1>num2){
+    console.log("num1isgreater")
+}else{
+    console.log("num2isgreater")
+}
+
+if(num1==num2){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+if(num1===num2){
+    console.log(true)
+}else{
+    console.log(false)
+}
