c597bca085d03214b3010a43aa23a95d02e52043
ravithejagolla <gollaravitheja007@gmail.com>
2024-10-25 21:17:09 +0530

    added

---
+++ q13.js
@@ -0,0 +1,11 @@
+let numValue=42
+
+let stringValue="42"
+
+let result=numValue+stringValue
+
+if(result>100){
+    console.log("The result is greaterthan 100")
+}else{
+    console.log("The result is lessthan or equal 100")
+}
